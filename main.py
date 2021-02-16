03fe89acacd662c4e85f3f5f284fa69a024b0b3b
bachirelkhadir <bachir009+git@gmail.com>
2021-02-16 18:27:14 -0500

    main.py

--- main.py
+++ main.py
@@ -3,9 +3,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib
-
+plt.style.use('./doomeone.mplstyle')
 print(plt.style.available)
-plt.style.use('doomeone')
 print(matplotlib.matplotlib_fname())
 plt.plot(np.random.rand(10), np.random.rand(10), label="legend")
 plt.plot(np.random.rand(10), np.random.rand(10), label="legend")
