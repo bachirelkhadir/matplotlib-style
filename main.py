9a226e0d4e5d6e7968197b0d9e6142c0167dee5e
bachirelkhadir <bachir009+git@gmail.com>
2021-02-16 18:27:32 -0500

    main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib
-plt.style.use(['./doomeone.mplstyle', 'ggplot'])
+plt.style.use(['ggplot', './doomeone.mplstyle', ])
 print(plt.style.available)
 print(matplotlib.matplotlib_fname())
 plt.plot(np.random.rand(10), np.random.rand(10), label="legend")
