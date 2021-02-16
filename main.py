f08af65c2c9324b255058b784e7ee638f4340f08
bachirelkhadir <bachir009+git@gmail.com>
2021-02-16 18:01:20 -0500

    main.py

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib
 
-
+matplotlib.matplotlib_fname()
 plt.plot(np.random.rand(10), np.random.rand(10), label="legend")
 plt.legend()
 plt.show()
